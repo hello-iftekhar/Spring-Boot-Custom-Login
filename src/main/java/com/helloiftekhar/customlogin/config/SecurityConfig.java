9a91cba5aa30972b9cd4eb906f9d70115a48b817
Iftekhar Hossain <helloiftekhar18@gmail.com>
2024-01-02 12:09:42 +0600

    initial commit

---
+++ src/main/java/com/helloiftekhar/customlogin/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.helloiftekhar.customlogin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(form->form
+                        .loginPage("/login").permitAll())
+                .authorizeHttpRequests(
+                        auth->auth
+                                .requestMatchers("/images/**").permitAll()
+                                .anyRequest().authenticated())
+                .build();
+    }
+}
---
+++ src/test/java/com/helloiftekhar/customlogin/CustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloiftekhar.customlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
